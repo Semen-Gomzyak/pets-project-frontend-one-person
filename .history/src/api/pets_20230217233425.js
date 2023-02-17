2f9dd65a378403dd0422822523f5640626148fb9
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-02-17 23:39:47 +0100

    add url

--- .history/src/api/pets_20230217233425.js
+++ .history/src/api/pets_20230217233425.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const { REACT_APP_BASE_URL } = process.env
 
 const getNews = async () => {
-    return axios.get(`${REACT_APP_BASE_URL}/api/news`)
+    return axios.get(`${REACT_APP_BASE_URL}/news`)
 }
